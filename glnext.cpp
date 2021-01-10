defc60ea06684bf71a77ba33a7abc297920e825a
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-01-10 09:40:41 +0200

    vulkan instance and command buffer

--- glnext.cpp
+++ glnext.cpp
@@ -1,5 +1,6 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <structmember.h>
 
 #include <vulkan/vulkan_core.h>
 
@@ -42,7 +43,7 @@ struct Context {
 
     uint32_t width;
     uint32_t height;
-    uint32_t samples;
+    VkSampleCountFlagBits samples;
 
     VkRenderPass render_pass;
     VkFramebuffer framebuffer;
@@ -60,6 +61,7 @@ struct Memory {
 
     VkBool32 host;
     VkDeviceSize size;
+    VkDeviceSize offset;
     VkDeviceMemory memory;
     void * ptr;
 };
@@ -125,23 +127,126 @@ PyTypeObject * Image_type;
 
 PyObject * empty_str;
 
-Memory * new_memory(Instance * self) {
+VkCommandBuffer begin_commands(Instance * self) {
+    VkCommandBufferBeginInfo command_buffer_begin_info = {
+        VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+        NULL,
+        VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
+        NULL,
+    };
+
+    vkBeginCommandBuffer(self->command_buffer, &command_buffer_begin_info);
+    return self->command_buffer;
+}
+
+void end_commands(Instance * self) {
+    vkEndCommandBuffer(self->command_buffer);
+
+    VkSubmitInfo submit_info = {
+        VK_STRUCTURE_TYPE_SUBMIT_INFO,
+        NULL,
+        0,
+        NULL,
+        NULL,
+        1,
+        &self->command_buffer,
+        0,
+        NULL,
+    };
+
+    vkQueueSubmit(self->queue, 1, &submit_info, self->fence);
+    vkWaitForFences(self->device, 1, &self->fence, true, UINT64_MAX);
+    vkResetFences(self->device, 1, &self->fence);
+}
+
+Memory * new_memory(Instance * self, bool host) {
     Memory * res = PyObject_New(Memory, Memory_type);
     res->instance = self;
+    res->host = host;
+    res->memory = NULL;
+    res->ptr = NULL;
+    res->offset = 0;
+    res->size = 0;
     PyList_Append(self->memory_list, (PyObject *)res);
     return res;
 }
 
-Buffer * new_buffer(Instance * self) {
+VkDeviceSize take_memory(Memory * self, VkMemoryRequirements requirements) {
+    if (VkDeviceSize padding = self->offset % requirements.alignment) {
+        self->offset += requirements.alignment - padding;
+    }
+    VkDeviceSize res = self->offset;
+    self->offset += requirements.size;
+    return res;
+}
+
+Buffer * new_buffer(Instance * self, VkDeviceSize size, VkBufferUsageFlags usage, Memory * memory) {
     Buffer * res = PyObject_New(Buffer, Buffer_type);
     res->instance = self;
+    res->memory = memory;
+    res->usage = usage;
+    res->buffer = NULL;
+    res->size = size;
+
+    if (!size) {
+        return res;
+    }
+
+    VkBufferCreateInfo buffer_create_info = {
+        VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+        NULL,
+        0,
+        size,
+        usage | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+        VK_SHARING_MODE_EXCLUSIVE,
+        0,
+        NULL,
+    };
+
+    vkCreateBuffer(self->device, &buffer_create_info, NULL, &res->buffer);
+
+    VkMemoryRequirements requirements = {};
+    vkGetBufferMemoryRequirements(self->device, res->buffer, &requirements);
+    res->offset = take_memory(memory, requirements);
+
     PyList_Append(self->buffer_list, (PyObject *)res);
     return res;
 }
 
-Image * new_image(Instance * self) {
+Image * new_image(Instance * self, VkDeviceSize size, VkExtent3D extent, VkFormat format, VkFlags usage, VkSampleCountFlagBits samples, Memory * memory) {
     Image * res = PyObject_New(Image, Image_type);
     res->instance = self;
+    res->memory = memory;
+    res->image = NULL;
+    res->size = size;
+    res->extent = extent;
+    res->samples = samples;
+    res->format = format;
+
+    VkImageCreateInfo image_create_info = {
+        VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+        NULL,
+        0,
+        VK_IMAGE_TYPE_2D,
+        format,
+        extent,
+        1,
+        1,
+        samples,
+        VK_IMAGE_TILING_OPTIMAL,
+        usage,
+        VK_SHARING_MODE_EXCLUSIVE,
+        0,
+        NULL,
+        VK_IMAGE_LAYOUT_UNDEFINED,
+    };
+
+    vkCreateImage(self->device, &image_create_info, NULL, &res->image);
+
+    VkMemoryRequirements requirements = {};
+    vkGetImageMemoryRequirements(self->device, res->image, &requirements);
+    res->offset = take_memory(memory, requirements);
+
     PyList_Append(self->image_list, (PyObject *)res);
     return res;
 }
@@ -176,10 +281,114 @@ Instance * glnext_meth_instance(PyObject * self, PyObject * args, PyObject * kwa
     res->buffer_list = PyList_New(0);
     res->image_list = PyList_New(0);
 
-    res->host_memory = new_memory(res);
-    res->device_memory = new_memory(res);
+    VkApplicationInfo application_info = {
+        VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        NULL,
+        "application",
+        0,
+        "engine",
+        0,
+        VK_API_VERSION_1_0,
+    };
 
-    res->host_buffer = new_buffer(res);
+    const char * instance_layer_array[] = {"VK_LAYER_KHRONOS_validation"};
+    const char * instance_extension_array[] = {"VK_EXT_debug_utils"};
+
+    VkInstanceCreateInfo instance_create_info = {
+        VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        NULL,
+        0,
+        &application_info,
+        1,
+        instance_layer_array,
+        1,
+        instance_extension_array,
+    };
+
+    vkCreateInstance(&instance_create_info, NULL, &res->instance);
+
+    uint32_t num_physical_devices = 1;
+    vkEnumeratePhysicalDevices(res->instance, &num_physical_devices, &res->physical_device);
+
+    VkPhysicalDeviceMemoryProperties device_memory_properties = {};
+    vkGetPhysicalDeviceMemoryProperties(res->physical_device, &device_memory_properties);
+
+    VkPhysicalDeviceFeatures supported_features = {};
+    vkGetPhysicalDeviceFeatures(res->physical_device, &supported_features);
+
+    for (uint32_t i = 0; i < device_memory_properties.memoryTypeCount; ++i) {
+        VkMemoryPropertyFlags flags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
+        if ((device_memory_properties.memoryTypes[i].propertyFlags & flags) == flags) {
+            res->host_memory_type_index = i;
+            break;
+        }
+    }
+
+    for (uint32_t i = 0; i < device_memory_properties.memoryTypeCount; ++i) {
+        VkMemoryPropertyFlags flags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+        if ((device_memory_properties.memoryTypes[i].propertyFlags & flags) == flags) {
+            res->device_memory_type_index = i;
+            break;
+        }
+    }
+
+    res->queue_family_index = 0;
+    float queue_priority = 1.0f;
+
+    VkDeviceQueueCreateInfo device_queue_create_info = {
+        VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        NULL,
+        0,
+        res->queue_family_index,
+        1,
+        &queue_priority,
+    };
+
+    VkPhysicalDeviceFeatures physical_device_features = {};
+    physical_device_features.multiDrawIndirect = supported_features.multiDrawIndirect;
+
+    VkDeviceCreateInfo device_create_info = {
+        VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+        NULL,
+        0,
+        1,
+        &device_queue_create_info,
+        0,
+        NULL,
+        0,
+        NULL,
+        &physical_device_features,
+    };
+
+    vkCreateDevice(res->physical_device, &device_create_info, NULL, &res->device);
+    vkGetDeviceQueue(res->device, res->queue_family_index, 0, &res->queue);
+
+    VkCommandPoolCreateInfo command_pool_create_info = {
+        VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+        NULL,
+        VK_COMMAND_POOL_CREATE_TRANSIENT_BIT | VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
+        res->queue_family_index,
+    };
+
+    vkCreateCommandPool(res->device, &command_pool_create_info, NULL, &res->command_pool);
+
+    VkCommandBufferAllocateInfo command_buffer_allocate_info = {
+        VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+        NULL,
+        res->command_pool,
+        VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+        1,
+    };
+
+    vkAllocateCommandBuffers(res->device, &command_buffer_allocate_info, &res->command_buffer);
+
+    VkFenceCreateInfo fence_create_info = {VK_STRUCTURE_TYPE_FENCE_CREATE_INFO, NULL, 0};
+    vkCreateFence(res->device, &fence_create_info, NULL, &res->fence);
+
+    res->host_memory = new_memory(res, true);
+    res->device_memory = new_memory(res, false);
+
+    res->host_buffer = new_buffer(res, host_memory_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, res->host_memory);
 
     return res;
 }
@@ -190,17 +399,46 @@ Context * Instance_meth_context(Instance * self, PyObject * args, PyObject * kwa
     uint32_t width;
     uint32_t height;
     const char * format;
-    uint32_t samples;
+    VkSampleCountFlagBits samples = VK_SAMPLE_COUNT_4_BIT;
+    Memory * memory = NULL;
 
-    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "(II)sI", keywords, &width, &height, &format, &samples)) {
+    int args_ok = PyArg_ParseTupleAndKeywords(
+        args,
+        kwargs,
+        "(II)s|IO!",
+        keywords,
+        &width,
+        &height,
+        &format,
+        &samples,
+        Memory_type,
+        &memory
+    );
+
+    if (!args_ok) {
         return NULL;
+    }
+
+    if (!memory) {
+        memory = new_memory(self, false);
     }
 
     Context * res = PyObject_New(Context, Context_type);
     res->instance = self;
 
-    res->uniform_buffer = new_buffer(self);
-    res->depth_image = new_image(self);
+    res->renderer_list = PyList_New(0);
+
+    res->uniform_buffer = new_buffer(self, 64 * 1024, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, memory);
+
+    res->depth_image = new_image(
+        self,
+        width * height * samples * 4,
+        {width, height, 1},
+        VK_FORMAT_D24_UNORM_S8_UINT,
+        VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+        samples,
+        memory
+    );
 
     res->width = width;
     res->height = height;
@@ -213,28 +451,54 @@ Context * Instance_meth_context(Instance * self, PyObject * args, PyObject * kwa
 Memory * Instance_meth_memory(Instance * self, PyObject * args, PyObject * kwargs) {
     static char * keywords[] = {"size", NULL};
 
-    uint64_t size;
+    VkDeviceSize size;
 
     if (!PyArg_ParseTupleAndKeywords(args, kwargs, "k", keywords, &size)) {
         return NULL;
     }
 
-    Memory * res = new_memory(self);
+    Memory * res = new_memory(self, false);
     return res;
 }
 
 Image * Instance_meth_image(Instance * self, PyObject * args, PyObject * kwargs) {
-    static char * keywords[] = {"size", "format", NULL};
+    static char * keywords[] = {"size", "format", "memory", NULL};
 
     uint32_t width;
     uint32_t height;
-    const char * format;
+    const char * format_str;
+    Memory * memory = NULL;
 
-    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "(II)s", keywords, &width, &height, &format)) {
+    int args_ok = PyArg_ParseTupleAndKeywords(
+        args,
+        kwargs,
+        "(II)s|O!",
+        keywords,
+        &width,
+        &height,
+        &format_str,
+        Memory_type,
+        &memory
+    );
+
+    if (!args_ok) {
         return NULL;
     }
 
-    Image * res = new_image(self);
+    if (!memory) {
+        memory = new_memory(self, false);
+    }
+
+    Image * res = new_image(
+        self,
+        width * height * 4,
+        {width, height, 1},
+        VK_FORMAT_B8G8R8A8_UNORM,
+        VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+        VK_SAMPLE_COUNT_1_BIT,
+        memory
+    );
+
     return res;
 }
 
@@ -261,11 +525,12 @@ Renderer * Context_meth_renderer(Context * self, PyObject * args, PyObject * kwa
     uint32_t index_count = 0;
     uint32_t indirect_count = 0;
     const char * topology_str = "triangles";
+    Memory * memory = NULL;
 
     int args_ok = PyArg_ParseTupleAndKeywords(
         args,
         kwargs,
-        "|$O!O!OOIIIIs",
+        "|$O!O!OOIIIIsO!",
         keywords,
         &PyBytes_Type,
         &vertex_shader,
@@ -277,20 +542,51 @@ Renderer * Context_meth_renderer(Context * self, PyObject * args, PyObject * kwa
         &instance_count,
         &index_count,
         &indirect_count,
-        &topology_str
+        &topology_str,
+        Memory_type,
+        &memory
     );
 
     if (!args_ok) {
         return NULL;
     }
 
+    if (!memory) {
+        memory = new_memory(self->instance, false);
+    }
+
     Renderer * res = PyObject_New(Renderer, Renderer_type);
     res->ctx = self;
+    res->memory = memory;
+    res->enabled = true;
 
-    res->vertex_buffer = new_buffer(self->instance);
-    res->instance_buffer = new_buffer(self->instance);
-    res->index_buffer = new_buffer(self->instance);
-    res->indirect_buffer = new_buffer(self->instance);
+    res->vertex_buffer = new_buffer(
+        self->instance,
+        vertex_count * 12, // TODO: from format
+        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+        memory
+    );
+
+    res->instance_buffer = new_buffer(
+        self->instance,
+        instance_count * 12, // TODO: from format
+        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+        memory
+    );
+
+    res->index_buffer = new_buffer(
+        self->instance,
+        index_count * 4,
+        VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+        memory
+    );
+
+    res->indirect_buffer = new_buffer(
+        self->instance,
+        indirect_count * 20, // TODO: indexed ? 20 : 16
+        VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT,
+        memory
+    );
 
     PyList_Append(self->renderer_list, (PyObject *)res);
     return res;
@@ -318,6 +614,12 @@ PyObject * Context_meth_render(Context * self, PyObject * args, PyObject * kwarg
     if (!PyArg_ParseTupleAndKeywords(args, kwargs, "|(II)", keywords, &width, &height)) {
         return NULL;
     }
+
+    VkCommandBuffer cmd = begin_commands(self->instance);
+
+    // TODO: draw
+
+    end_commands(self->instance);
 
     Py_RETURN_NONE;
 }
@@ -376,14 +678,46 @@ PyObject * Renderer_meth_update(Renderer * self, PyObject * args, PyObject * kwa
     Py_RETURN_NONE;
 }
 
+PyObject * Image_meth_write(Image * self, PyObject * args, PyObject * kwargs) {
+    static char * keywords[] = {"data", NULL};
+
+    PyObject * data;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "O", keywords, &data)) {
+        return NULL;
+    }
+
+    VkCommandBuffer cmd = begin_commands(self->instance);
+
+    // TODO: layout transition and copy buffer to image
+
+    end_commands(self->instance);
+    Py_RETURN_NONE;
+}
+
+PyObject * Instance_meth_info(Instance * self) {
+    Py_RETURN_NONE;
+}
+
+PyObject * Instance_meth_release(Instance * self) {
+    Py_RETURN_NONE;
+}
+
 void default_dealloc(PyObject * self) {
     Py_TYPE(self)->tp_free(self);
 }
+
+PyMemberDef Instance_members[] = {
+    {"device_memory", T_OBJECT, offsetof(Instance, device_memory), READONLY, NULL},
+    {},
+};
 
 PyMethodDef Instance_methods[] = {
     {"context", (PyCFunction)Instance_meth_context, METH_VARARGS | METH_KEYWORDS, NULL},
     {"memory", (PyCFunction)Instance_meth_memory, METH_VARARGS | METH_KEYWORDS, NULL},
     {"image", (PyCFunction)Instance_meth_image, METH_VARARGS | METH_KEYWORDS, NULL},
+    {"release", (PyCFunction)Instance_meth_release, METH_NOARGS, NULL},
+    {"info", (PyCFunction)Instance_meth_info, METH_NOARGS, NULL},
     {},
 };
 
@@ -396,6 +730,11 @@ PyMethodDef Context_methods[] = {
 
 PyMethodDef Renderer_methods[] = {
     {"update", (PyCFunction)Renderer_meth_update, METH_VARARGS | METH_KEYWORDS, NULL},
+    {},
+};
+
+PyMethodDef Image_methods[] = {
+    {"write", (PyCFunction)Image_meth_write, METH_VARARGS | METH_KEYWORDS, NULL},
     {},
 };
 
@@ -428,6 +767,7 @@ PyType_Slot Buffer_slots[] = {
 };
 
 PyType_Slot Image_slots[] = {
+    {Py_tp_methods, Image_methods},
     {Py_tp_dealloc, default_dealloc},
     {},
 };
@@ -455,6 +795,8 @@ extern "C" PyObject * PyInit_glnext() {
     Renderer_type = (PyTypeObject *)PyType_FromSpec(&Renderer_spec);
     Buffer_type = (PyTypeObject *)PyType_FromSpec(&Buffer_spec);
     Image_type = (PyTypeObject *)PyType_FromSpec(&Image_spec);
+
+    empty_str = PyUnicode_FromString("");
 
     return module;
 }
