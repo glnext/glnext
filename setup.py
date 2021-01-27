25a1520669195621f17ff26542e2a7f13d896bb4
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-01-27 07:53:07 +0200

    vulkan headers

--- setup.py
+++ setup.py
@@ -12,17 +12,14 @@ for known in PLATFORMS:
         break
 
 define_macros = [('BUILD_' + target.upper(), None)]
+include_dirs = ['./include']
 extra_compile_args = []
 extra_link_args = []
-include_dirs = []
-library_dirs = []
 libraries = []
 
-if target == 'windows':
-    # extra_compile_args.append('/Z7')
-    # extra_link_args.append('/DEBUG:FULL')
-    include_dirs.append(os.path.join(os.getenv('VULKAN_SDK'), 'Include'))
-    library_dirs.append(os.path.join(os.getenv('VULKAN_SDK'), 'Lib'))
+# if target == 'windows':
+#     extra_compile_args.append('/Z7')
+#     extra_link_args.append('/DEBUG:FULL')
 
 if target == 'linux':
     extra_compile_args.append('-fpermissive')
@@ -47,7 +44,6 @@ glnext = Extension(
     extra_compile_args=extra_compile_args,
     extra_link_args=extra_link_args,
     include_dirs=include_dirs,
-    library_dirs=library_dirs,
     libraries=libraries,
 )
 
