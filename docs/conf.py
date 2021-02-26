39dbe3d9360068970aab63b5579d0af3e951685a
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-02-26 08:27:45 +0200

    initialize documentation

---
+++ docs/conf.py
@@ -0,0 +1,21 @@
+project = 'glnext'
+copyright = '2021, Dombi Szabolcs'
+author = 'Dombi Szabolcs'
+
+release = '0.7.0'
+
+extensions = [
+    'sphinx_rtd_theme',
+]
+
+templates_path = []
+
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+
+html_static_path = ['static']
+
+
+def setup(app):
+    app.add_stylesheet('css/custom.css')
