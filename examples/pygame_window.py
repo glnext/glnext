fdf94a4ae92552937de2ef8a8b4ddae8cf6b5887
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-03-22 17:43:26 +0200

    pygame example window

---
+++ examples/pygame_window.py
@@ -0,0 +1,62 @@
+import glnext
+import pygame
+from glnext_compiler import glsl
+
+instance = glnext.instance(surface=True)
+task = instance.task()
+
+framebuffer = task.framebuffer((512, 512))
+
+pipeline = framebuffer.render(
+    vertex_shader=glsl('''
+        #version 450
+        #pragma shader_stage(vertex)
+
+        layout (location = 0) out vec4 out_color;
+
+        vec2 positions[3] = vec2[](
+            vec2(-0.5, -0.5),
+            vec2(0.5, -0.5),
+            vec2(0.0, 0.5)
+        );
+
+        vec4 colors[3] = vec4[](
+            vec4(1.0, 0.0, 0.0, 1.0),
+            vec4(0.0, 1.0, 0.0, 1.0),
+            vec4(0.0, 0.0, 1.0, 1.0)
+        );
+
+        void main() {
+            gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
+            out_color = colors[gl_VertexIndex];
+            gl_Position.y *= -1.0;
+        }
+    '''),
+    fragment_shader=glsl('''
+        #version 450
+        #pragma shader_stage(fragment)
+
+        layout (location = 0) in vec4 in_color;
+        layout (location = 0) out vec4 out_color;
+
+        void main() {
+            out_color = in_color;
+        }
+    '''),
+    vertex_count=3,
+)
+
+pygame.init()
+pygame.display.set_mode((512, 512))
+
+handle = (pygame.display.get_wm_info()['hinstance'], pygame.display.get_wm_info()['window'])
+instance.surface(handle, framebuffer.output[0])
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    task.run()
+    instance.present()
